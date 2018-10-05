dd5ec7854b04b0c98b020f69ebc42515e367a37b
MAYTINH-8JCGDJ3\MyPC <luutien18195@gmail.com>
2018-10-05 08:41:18 +0700

    init project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        Rectangle rec1 = new Rectangle(4,40);
+        Rectangle rec2 = new Rectangle(3.5,35.9);
+
+        rec1.display();
+        rec2.display();
+    }
+}
